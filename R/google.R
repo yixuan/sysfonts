943037041bf0891c5b3a8e1fce4b7a2fc132ce22
Yixuan Qiu <yixuan.qiu@cos.name>
2014-02-22 17:44:24 -0500

    rewrite Google Fonts functions

--- R/google.R
+++ R/google.R
@@ -1,15 +1,47 @@
-# something like
-# @font-face {
-#     font-family: 'Coming Soon';
-#     font-style: normal;
-#     font-weight: 400;
-#     src: local('Coming Soon'), local('ComingSoon'), url(http://themes.googleusercontent.com/static/fonts/comingsoon/v3/myblyOycMnPMGjfPG-DzP4bN6UDyHWBl620a-IRfuBk.woff) format('woff');
-# }
-.parse.url = function(str)
+## Object to store the Google Fonts database
+.pkg.env$.google.db = NULL;
+
+.google.font.db = function()
 {
-    line = grep("url\\(http://.*\\)", str, value = TRUE);
-    url = gsub(".*url\\(([^\\)]*)\\).*", "\\1", line);
-    return(url);
+    ## If database already exists, return it
+    if(!is.null(.pkg.env$.google.db))
+        return(.pkg.env$.google.db);
+    
+    ## Else, download it
+    baseurl = "https://www.googleapis.com/webfonts/v1/webfonts";
+    key = "AIzaSyDilHY_z1p7WltVNj5gOEHVHD3AIpW8R4o";
+    apiurl = sprintf("%s?key=%s", baseurl, key);
+    ret = getURLContent(apiurl, ssl.verifypeer = FALSE);
+    res = fromJSON(ret, FALSE);
+    .pkg.env$.google.db = res;
+    return(res);
+}
+
+.google.font.list = function()
+{
+    db = .google.font.db();
+    family = sapply(db[[2]], function(x) x$family);
+    category = sapply(db[[2]], function(x) x$category);
+    return(data.frame(family = family, category = category,
+                      stringsAsFactors = FALSE));
+}
+
+.search.db = function(family)
+{
+    gflist = .google.font.list();
+    gffamily = gflist$family;
+    ind = grep(sprintf("^%s$", family), gffamily, ignore.case = TRUE);
+    if(!length(ind))
+    {
+        ind = grep(family, gffamily, ignore.case = TRUE);
+        if(length(ind))
+        {
+            cat("Do you mean one of the following font(s)?\n");
+            cat(paste("  ", gffamily[ind], collapse = "\n"), "\n");
+        }
+        stop("font not found");
+    }
+    return(ind);
 }
 
 # download font file and return the path of destination
@@ -21,13 +53,48 @@
     return(path);
 }
 
+
+
+#' List available fonts in Google Fonts
+#' 
+#' This function lists family names of the fonts that are currently
+#' available in Google Fonts. When running this function for the first time, 
+#' it may take a few seconds to fetch the font information database.
+#' This function requires the packages \pkg{RCurl} and \pkg{jsonlite} to be
+#' installed.
+#' 
+#' @return A character vector of available font family names in Google Fonts
+#' 
+#' @seealso \code{\link{font.add.google}()}
+#' 
+#' @export
+#' 
+#' @author Yixuan Qiu <\url{http://yixuan.cos.name/}>
+#' 
+#' @examples \dontrun{
+#' font.families.google()
+#' }
+#' 
+font.families.google = function()
+{
+    if(!require("RCurl"))
+        stop("cannot load 'RCurl' package");
+    if(!require("jsonlite"))
+        stop("cannot load 'jsonlite' package");
+    
+    return(.google.font.list()$family);
+}
+
+
 #' Download and add Google Fonts
 #' 
 #' This function will search the Google Fonts repository for a specified
 #' family name, download the proper font files and then add them to R.
+#' This function requires the packages \pkg{RCurl} and \pkg{jsonlite} to be
+#' installed.
 #' 
-#' @param family family name that will be searched in Google Fonts. Case
-#'               sensitive.
+#' @param name name of the font that will be searched in Google Fonts
+#' @param family family name of the font that will be used in R
 #' @param regular.wt font weight for the regular font face, usually 400
 #' @param bold.wt font weight for the bold font face, usually 700
 #' 
@@ -43,10 +110,12 @@
 #' 
 #' @export
 #' 
+#' @seealso \code{\link{font.families.google}()}
+#' 
 #' @author Yixuan Qiu <\url{http://yixuan.cos.name/}>
 #' 
 #' @examples \dontrun{
-#' font.google("Alegreya Sans");
+#' font.add.google("Alegreya Sans", "aleg");
 #' 
 #' if(require(showtext))
 #' {
@@ -54,7 +123,7 @@
 #'     pdf("google-fonts-ex.pdf");
 #'     showtext.begin();
 #'     
-#'     par(family = "Alegreya Sans");
+#'     par(family = "aleg");
 #'     plot(0:5,0:5, type="n");
 #'     text(1:4, 1:4, "Alegreya Sans", font=1:4, cex = 2);
 #'     
@@ -64,51 +133,50 @@
 #' }
 #' 
 #' }
-font.google = function(family, regular.wt = 400, bold.wt = 700)
+font.add.google = function(name, family = name, regular.wt = 400,
+                           bold.wt = 700)
 {
-    regular.wt = as.integer(regular.wt);
-    bold.wt = as.integer(bold.wt);
-    familysp = family;
-    family = gsub(" ", "+", family);
+    if(!require("RCurl"))
+        stop("cannot load 'RCurl' package");
+    if(!require("jsonlite"))
+        stop("cannot load 'jsonlite' package");
+    
+    name = as.character(name);
+    
+    db = .google.font.db();
+    ind = .search.db(name);
+    font = db[[2]][[ind]];
+    
+    ## names of type variants to search in the db
+    ## e.g., "regular", "700", "italic", 700italic", etc.
+    regular = as.character(regular.wt);
+    bold = as.character(bold.wt);
+    italic = paste(regular, "italic", sep = "");
+    bolditalic = paste(bold, "italic", sep = "");
+    if(regular.wt == 400)
+    {
+        regular = "regular";
+        italic = "italic"
+    }
     
     ## download regular font face
-    r.page = sprintf("http://fonts.googleapis.com/css?family=%s:%d",
-                     family, regular.wt);
-    link = url(r.page);
-    r.url = tryCatch(.parse.url(readLines(link)),
-                     error = function(e) {stop("font not found")});
-    close(link);
+    r.url = font$files[[regular]];
+    if(is.null(r.url))
+        stop(sprintf("regular(weight=%d) variant of '%s' font not found",
+                     regular.wt, name));
     r.file = .download.file(r.url);
     
     ## download bold font face
-    b.page = sprintf("http://fonts.googleapis.com/css?family=%s:%d",
-                     family, bold.wt);
-    link = url(b.page);
-    b.url = tryCatch(.parse.url(readLines(link)),
-                     warning = function(w) NULL,
-                     error = function(e) NULL);
-    close(link);
+    b.url = font$files[[bold]];
     b.file = if(is.null(b.url)) NULL else .download.file(b.url);
     
     ## download italic font face
-    i.page = sprintf("http://fonts.googleapis.com/css?family=%s:%ditalic",
-                     family, regular.wt);
-    link = url(i.page);
-    i.url = tryCatch(.parse.url(readLines(link)),
-                     warning = function(w) NULL,
-                     error = function(e) NULL);
-    close(link);
+    i.url = font$files[[italic]];
     i.file = if(is.null(i.url)) NULL else .download.file(i.url);
     
     ## download bold-italic font face
-    bi.page = sprintf("http://fonts.googleapis.com/css?family=%s:%ditalic",
-                      family, bold.wt);
-    link = url(bi.page);
-    bi.url = tryCatch(.parse.url(readLines(link)),
-                     warning = function(w) NULL,
-                     error = function(e) NULL);
-    close(link);
+    bi.url = font$files[[bolditalic]];
     bi.file = if(is.null(bi.url)) NULL else .download.file(bi.url);
-    
-    font.add(familysp, r.file, b.file, i.file, bi.file);
+
+    font.add(family, r.file, b.file, i.file, bi.file);
 }
