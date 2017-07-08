238a5f60d9c09f9356bf5b9c835d7627ec6dbddb
Yixuan Qiu <yixuanq@gmail.com>
2017-07-08 09:56:59 -0400

    refine code and documentation

--- R/google.R
+++ R/google.R
@@ -78,30 +78,49 @@ search_db = function(family)
 download_font_file = function(url, repo = "http://fonts.gstatic.com/")
 {
     ## We need to use curl package here
-    url = gsub("http://fonts.gstatic.com/", repo, url)
     
     dest = file.path(tempdir(), basename(url))
-    curl::curl_download(url, dest)
+    
+    if(repo != "http://fonts.gstatic.com/")
+    {
+        url_repo = gsub("http://fonts.gstatic.com/", repo, url)
+        tryCatch(
+            
+            ## Try the user-specified repository
+            {
+                curl::curl_download(url_repo, dest)
+            },
+            
+            ## If not successful, use the default one
+            error = function(e) {
+                message("font not found in the user-provided repo, try default repo...")
+                curl::curl_download(url, dest)
+            }
+            
+        )
+    } else {
+        curl::curl_download(url, dest)
+    }
     
     dest
 }
 
 
 
-#' List available fonts in Google Fonts
+#' List Font Families Available in Google Fonts
 #' 
 #' This function lists family names of the fonts that are currently
 #' available in Google Fonts. When running this function for the first time, 
 #' it may take a few seconds to fetch the font information database.
-#' This function requires the \pkg{jsonlite} and \pkg{RCurl} packages.
+#' This function requires the \pkg{jsonlite} and \pkg{curl} packages.
 #' 
-#' @return A character vector of available font family names in Google Fonts
+#' @return A character vector of available font family names in Google Fonts.
 #' 
 #' @seealso \code{\link{font.add.google}()}
 #' 
 #' @export
 #' 
-#' @author Yixuan Qiu <\url{http://yixuan.cos.name/}>
+#' @author Yixuan Qiu <\url{http://statr.me/}>
 #' 
 #' @examples \dontrun{
 #' font.families.google()
@@ -109,103 +128,100 @@ download_font_file = function(url, repo = "http://fonts.gstatic.com/")
 #' 
 font.families.google = function()
 {
-    return(.google.font.list()$family);
+    google_font_list()$family
 }
 
 
-#' Download and add Google Fonts
+#' Load Google Fonts into 'sysfonts'
 #' 
 #' This function will search the Google Fonts repository
-#' (\url{http://www.google.com/fonts}) for a specified
-#' family name, download the proper font files and then add them to R.
-#' This function requires the \pkg{jsonlite} and \pkg{RCurl} packages.
+#' (\url{https://fonts.google.com/}) for a specified
+#' family name, download the proper font files, and then add them to \pkg{sysfonts}.
+#' This function requires the \pkg{jsonlite} and \pkg{curl} packages.
 #' 
 #' @param name name of the font that will be searched in Google Fonts
 #' @param family family name of the font that will be used in R
 #' @param regular.wt font weight for the regular font face, usually 400
 #' @param bold.wt font weight for the bold font face, usually 700
-#' @param repo the site to download fonts from. "google" indicates
-#'             downloading from the original Google Fonts repository.
-#'             "useso" indicates downloading from a proxy server
-#'             provided by \url{http://fonts.useso.com}. If the default
-#'             choice "google" fails, try to set \code{repo = "useso"}
+#' @param repo the site that hosts the font files. Default is the official
+#'             repository \code{http://fonts.gstatic.com/} provided by
+#'             Google Fonts.
 #' 
 #' @details There are hundreds of open source fonts in the Google Fonts
-#'          repository (\url{http://www.google.com/fonts}).
+#'          repository (\url{https://fonts.google.com/}).
 #'          This function will try to search the font family specified
-#'          by the \code{family} argument, and then automatically
+#'          by the \code{name} argument, and then automatically
 #'          download the font files for all possible font faces
 #'          ("regular", "bold", "italic" and "bold italic",
 #'          but no"symbol").
 #'          If fonts are found and downloaded successfully, they will be
-#'          also added to R with the given family name.
+#'          also added to \pkg{sysfonts} with the given family name.
 #' 
 #' @export
 #' 
 #' @seealso \code{\link{font.families.google}()}
 #' 
-#' @author Yixuan Qiu <\url{http://yixuan.cos.name/}>
+#' @author Yixuan Qiu <\url{http://statr.me/}>
 #' 
 #' @examples \dontrun{
-#' font.add.google("Alegreya Sans", "aleg");
+#' font.add.google("Alegreya Sans", "aleg")
 #' 
 #' if(require(showtext))
 #' {
-#'     wd = setwd(tempdir());
-#'     pdf("google-fonts-ex.pdf");
-#'     showtext.begin();
+#'     wd = setwd(tempdir())
+#'     pdf("google-fonts-ex.pdf")
+#'     showtext.begin()
 #'     
-#'     par(family = "aleg");
-#'     plot(0:5,0:5, type="n");
-#'     text(1:4, 1:4, "Alegreya Sans", font=1:4, cex = 2);
+#'     par(family = "aleg")
+#'     plot(0:5,0:5, type="n")
+#'     text(1:4, 1:4, "Alegreya Sans", font=1:4, cex = 2)
 #'     
-#'     showtext.end();
-#'     dev.off();
-#'     setwd(wd);
+#'     showtext.end()
+#'     dev.off()
+#'     setwd(wd)
 #' }
 #' 
 #' }
 font.add.google = function(name, family = name, regular.wt = 400,
-                           bold.wt = 700, repo = c("google", "useso"))
+                           bold.wt = 700, repo = "http://fonts.gstatic.com/")
 {   
-    name = as.character(name)[1];
-    family = as.character(family)[1];
-    repo = as.character(repo)[1];
+    name   = as.character(name)[1]
+    family = as.character(family)[1]
+    repo   = as.character(repo)[1]
     
-    db = .google.font.db();
-    ind = .search.db(name);
-    font = db[[2]][[ind]];
+    db = google_font_db()
+    ind = search_db(name)
+    font = db[[2]][[ind]]
     
-    ## names of type variants to search in the db
+    ## Names of type variants to search in the db
     ## e.g., "regular", "700", "italic", 700italic", etc.
-    regular = as.character(regular.wt);
-    bold = as.character(bold.wt);
-    italic = paste(regular, "italic", sep = "");
-    bolditalic = paste(bold, "italic", sep = "");
+    regular = as.character(regular.wt)
+    bold = as.character(bold.wt)
+    italic = paste(regular, "italic", sep = "")
+    bolditalic = paste(bold, "italic", sep = "")
     if(regular.wt == 400)
     {
-        regular = "regular";
+        regular = "regular"
         italic = "italic"
     }
     
-    ## download regular font face
-    r.url = font$files[[regular]];
+    ## Download regular font face
+    r.url = font$files[[regular]]
     if(is.null(r.url))
-        stop(sprintf("regular(weight=%d) variant of '%s' font not found",
-                     regular.wt, name));
-    r.file = .download.file(r.url, repo);
+        stop(sprintf("regular (weight=%d) variant of '%s' font not found", regular.wt, name))
+    r.file = download_font_file(r.url, repo)
     
-    ## download bold font face
-    b.url = font$files[[bold]];
-    b.file = if(is.null(b.url)) NULL else .download.file(b.url, repo);
+    ## Download bold font face
+    b.url = font$files[[bold]]
+    b.file = if(is.null(b.url)) NULL else download_font_file(b.url, repo)
     
-    ## download italic font face
-    i.url = font$files[[italic]];
-    i.file = if(is.null(i.url)) NULL else .download.file(i.url, repo);
+    ## Download italic font face
+    i.url = font$files[[italic]]
+    i.file = if(is.null(i.url)) NULL else download_font_file(i.url, repo)
     
-    ## download bold-italic font face
-    bi.url = font$files[[bolditalic]];
-    bi.file = if(is.null(bi.url)) NULL else .download.file(bi.url, repo);
+    ## Download bold-italic font face
+    bi.url = font$files[[bolditalic]]
+    bi.file = if(is.null(bi.url)) NULL else download_font_file(bi.url, repo)
 
-    font.add(family, r.file, b.file, i.file, bi.file);
+    font.add(family, r.file, b.file, i.file, bi.file)
 }
